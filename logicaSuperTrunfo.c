b4693249993db3251b0076f98095de06c77f1b01
Franklin Andrade <100174287+frk-andrade@users.noreply.github.com>
2025-07-30 17:44:57 +0000

    Implementação da lógica do nível aventureiro

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -21,6 +21,79 @@ int main() {
     // 
     // (Repita para cada propriedade)
 
+    //declaração das variáveis
+    char letra_estado1, letra_estado2;
+    char cod_carta1[4], cod_carta2[4];
+    char nome_cidade1[30], nome_cidade2[30];
+    unsigned long int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int p_turistico1, p_turistico2;
+
+    //Lógica para obtenção das informações da carta 1
+    printf("Bem-vindo ao Desafio Super Trunfo - Países!\n");
+    printf("Vamos começar cadastrando a Carta 1! \n");
+    printf("Informe letra do Estado no intervalo de A a H: \n");
+    scanf("%c", letra_estado1);
+    printf("Informe novamente a letra do Estado, seguida do número no intervalo entre 01 a 04: \n");
+    scanf("%s", cod_carta1);
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", nome_cidade1);
+    printf("Informe a população: \n");
+    scanf("%lu", &populacao1);
+    printf("Informe o tamanho da cidade em km²: \n");
+    scanf("%f", &area1);
+    printf("Informe o PIB: \n");
+    scanf("%f", &pib1);
+    printf("Informe a quantidade de pontos turísticos: \n");
+    scanf("%d", &p_turistico1);
+
+    //Nível aventureiro
+    //Calculando a densidade populacional
+    float densidade1 = populacao1 / area1;
+    //Calculando o PIB per capta
+    float pib_capita1 = (float) (pib1 * 1000000000.0) / populacao1;
+
+    //Exibição dos resultados de nível aventureiro
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pib_capita1);
+
+    //Nível mestre
+    //Calculando o super poder
+    float superPoder1 = (float) populacao1 + area1 + pib1 + p_turistico1 + pib_capita1 + (1.0 / densidade1);
+
+    //Lógica para obtenção das informações da carta 2
+    printf("Agora a Carta 2! \n");
+    printf("Informe letra do Estado no intervalo de A a H: \n");
+    scanf("%c", letra_estado2 ); 
+    while (getchar() != '\n');
+    printf("Informe novamente a letra do Estado, seguida do número no intervalo entre 01 a 04: \n");
+    scanf("%s", cod_carta2);
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", nome_cidade2);
+    printf("Informe a população: \n");
+    scanf("%lu", &populacao2);
+    printf("Informe o tamanho da cidade em km²: \n");
+    scanf("%f", &area2);
+    printf("Informe o PIB: \n");
+    scanf("%f", &pib2);
+    printf("Informe a quantidade de pontos turísticos: \n");
+    scanf("%d", &p_turistico2);
+
+    //Nível aventureiro
+    //Calculando a densidade populacional
+    float densidade2 = populacao2 / area2;
+    //Calculando o PIB per capta
+    float pib_capita2 = (float) (pib2 * 1000000000.0) / populacao2;
+
+    //Exibição dos resultados de nível aventureiro
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pib_capita2);
+
+    //Nível mestre
+    //Calculando o super poder
+    float superPoder2 = (float) populacao2 + area2 + pib2 + p_turistico2 + pib_capita2 + (1.0 / densidade2);
+
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
@@ -31,6 +104,18 @@ int main() {
     // } else {
     //     printf("Cidade 2 tem maior população.\n");
     // }
+
+    if (populacao1 > populacao1)
+    {
+        printf("Carta 1 %s tem a maior população\n", nome_cidade1);
+    } else if (populacao1 == populacao2)
+    {
+        printf("Empate!\n");
+    } else {
+        printf("Carta 2 %s tem a maior população\n", nome_cidade2);
+    }
+    
+    
 
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
